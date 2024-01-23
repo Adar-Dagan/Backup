d63642574a56d26ea77f70a82bce8f841d167363
Adar Dagan <AdarDagan@outlook.com>
2024-01-23 14:34:17 +0200

    Fix template

--- program_template.sh
+++ program_template.sh
@@ -6,7 +6,9 @@ eval dir_path=$dir_path
 # Check the first argument
 if [ "$1" == "backup" ]; then
     # Add your backup code here
+    :
 elif [ "$1" == "recover" ]; then
     # Add your recover code here
+    :
 fi
 
